2e1c8a1c731c59687abf383fa6957c47e656ca9e
yl657 <yl657@duke.edu>
2020-11-16 18:12:39 -0500

    use pivot root

--- mount.cpp
+++ mount.cpp
@@ -2,12 +2,13 @@
 
 int mounts(){
   // mount root fs
+  fprintf(stdout, "Mounting new root fs\n");
   int mount_rc;
-  if (mount_rc = chroot("/contained")) {
+  if (mount_rc = pivot_root("/contained", "/tmp/old_root")) {
 		return mount_rc;
 	}
-  if (mount_rc = chdir("/")) {
-		return mount_rc;
-	}
+  // if (mount_rc = chdir("/")) {
+	// 	return mount_rc;
+	// }
   return 0;
 }
