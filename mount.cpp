62543facf7f846f74d07068be54f1c33a03bfa67
yl657 <yl657@duke.edu>
2020-11-16 18:15:47 -0500

    change to syscall

--- mount.cpp
+++ mount.cpp
@@ -4,7 +4,7 @@ int mounts(){
   // mount root fs
   fprintf(stdout, "Mounting new root fs\n");
   int mount_rc;
-  if (mount_rc = pivot_root("/contained", "/tmp/old_root")) {
+  if (mount_rc = syscall(SYS_pivot_root, ("/contained", "/tmp/old_root"))) {
 		return mount_rc;
 	}
   // if (mount_rc = chdir("/")) {
