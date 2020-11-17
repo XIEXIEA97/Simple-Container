38a6d0de0de7cc25e432ed6466933212386d9c47
root <root@vcm-16422.vm.duke.edu>
2020-11-17 17:48:38 -0500

    add filesystem for container

--- mount.cpp
+++ mount.cpp
@@ -1,28 +1,37 @@
 #include "sc.hpp"
 #include <dirent.h>
+#include <sys/mount.h>
 
 int mounts(){
   // mount root fs
   fprintf(stdout, "Mounting new root fs\n");
   int mount_rc;
-  if (mount_rc = chroot("/contained")) {
-		return mount_rc;
-	}
-  if (mount_rc = chdir("/")) {
+  // if (mount_rc = mount("proc", "proc", "/proc", 0, "")){
+  //   fprintf(stdout, "MOUNT FAILED\n");
+  //   return mount_rc;
+  // }
+
+  if (mount_rc = chroot("rootfs")) {
+    fprintf(stdout, "CHROOT FAILED\n");
 		return mount_rc;
 	}
 
-  DIR *d;
-  struct dirent *dir;
-  d = opendir("/bin");
-  if (d)
-  {
-      while ((dir = readdir(d)) != NULL)
-      {
-          fprintf(stdout, "%s\n", dir->d_name);
-      }
-      closedir(d);
-  }
+  if (mount_rc = chdir("/")) {
+    fprintf(stdout, "CHDIR FAILED\n");
+		return mount_rc;
+	}
+
+  // DIR *d;
+  // struct dirent *dir;
+  // d = opendir("/bin");
+  // if (d)
+  // {
+  //     while ((dir = readdir(d)) != NULL)
+  //     {
+  //         fprintf(stdout, "%s\n", dir->d_name);
+  //     }
+  //     closedir(d);
+  // }
 
   return 0;
 }
---
+++ rootfs/root/.bash_history
@@ -0,0 +1,15 @@
+ls
+exit
+ls
+cd root/
+ls
+cd ..
+chmod 755 root/
+chmod 755 root/.bashrc
+exit
+ls
+chmod 755 root/
+sudo chmod 755 root/
+exit
+ls
+exit
